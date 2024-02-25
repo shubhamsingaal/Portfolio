c0ece14534b225ffb550abf38ced9edc8fe6fe99
Shubham Kumar <shubhamkraj200@gmail.com>
2024-02-25 09:32:58 +0530

    fix:contact required field

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -89,6 +89,7 @@ const Contact = () => {
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
                       name='name'
+                      required
                     />
                   </div>
                   <div className='flex flex-col'>
@@ -99,6 +100,7 @@ const Contact = () => {
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
                       name='phone'
+                      
                     />
                   </div>
                 </div>
@@ -108,6 +110,7 @@ const Contact = () => {
                     className='border-2 rounded-lg p-3 flex border-gray-300'
                     type='email'
                     name='email'
+                    required
                   />
                 </div>
                 <div className='flex flex-col py-2'>
@@ -116,6 +119,7 @@ const Contact = () => {
                     className='border-2 rounded-lg p-3 flex border-gray-300'
                     type='text'
                     name='subject'
+                    required
                   />
                 </div>
                 <div className='flex flex-col py-2'>
@@ -124,6 +128,7 @@ const Contact = () => {
                     className='border-2 rounded-lg p-3 border-gray-300'
                     rows='4'
                     name='message'
+                    required
                   ></textarea>
                 </div>
                 <button className='w-full p-4 text-gray-100 mt-4'>

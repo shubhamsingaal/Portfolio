8b12553799118f927699a2d1539ebd801621b362
Shubham Kumar <sk.21u10084@btech.nitdgp.ac.in>
2024-08-25 19:44:15 +0530

    updated title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Shubham | Front-End Developer</title>
+        <title>Shubham | Full-Stack Developer</title>
         <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
         <link rel="icon" href="/fav.png" />
       </Head>
